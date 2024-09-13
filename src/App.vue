0cfe7e5a25daa685bf706e641c051513f86de77d
busefilizler <busefilizler@hotmail.com>
2024-09-13 11:44:21 +0300

    Refactor styles and add font family to the dashboard layout and app components

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
-import DashboardPage from './pages/DashboardPage.vue'
+import DashboardPage from "./pages/DashboardPage.vue";
 </script>
 
 <template>
-  <div class="w-screen h-screen">
+  <div class="w-screen h-screen p-6">
+    <div class="text-4xl text-violet-900 font-bold">My Dashboard My World!</div>
     <DashboardPage />
   </div>
 </template>
+<style>
+* {
+  font-family: "Poppins", sans-serif;
+}
+</style>
--- src/layouts/DashboardLayout.vue
+++ src/layouts/DashboardLayout.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="w-full min-w-[1200px] h-full flex flex-col px-10 border-4">
+  <div class="w-full min-w-[1200px] h-full flex flex-col">
     <div class="widgetArea w-full h-1/5">
       <slot name="widgetSide"></slot>
     </div>
 
-    <div class="dashboardArea !w-full min-w-[1200px] h-4/5 bg-violet-50 rounded-md border-violet-900 border">
+    <div class="dashboardArea !w-full min-w-[1200px] h-4/5 bg-violet-50 rounded-md border-violet-900 border mb-6">
       <div
         style="box-shadow: 0px 10px 15px #1741711a"
         class="w-full h-full"
